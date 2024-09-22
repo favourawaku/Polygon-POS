e4eb8142ff2230725062cf8f5eab21390529e8ba
favourawaku <sabofavour4@gmail.com>
2024-09-22 13:52:35 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    mumbai: {
-      url: process.env.MUMBAI_RPC_URL,
+    amoy: {
+      url: process.env.AMOY_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
     sepolia: {
